cf12f9c918b0ca2640c5651288aef4e8a011b76f
Jianchao.ma <Jianchao.ma@aliyun.com>
2020-04-04 16:17:58 +0800

    go-serverv0.7 读写分离模型

--- gnet/connection.go
+++ gnet/connection.go
@@ -21,6 +21,9 @@ type Connection struct {
 
 	//告知该链接已经退出/停止的channel
 	ExitBuffChan chan bool
+
+	//无缓冲管道，用于读、写两个goroutine之间的消息通信
+	msgChan chan []byte
 }
 
 //创建连接的方法
@@ -31,6 +34,7 @@ func NewConnection(conn *net.TCPConn, connID uint32, msgHandler gface.IMsgHandle
 		isClosed:     false,
 		MsgHandler:   msgHandler,
 		ExitBuffChan: make(chan bool, 1),
+		msgChan:      make(chan []byte),
 	}
 
 	return c
@@ -81,10 +85,33 @@ func (c *Connection) StartReader() {
 	}
 }
 
+/*
+   写消息Goroutine， 用户将数据发送给客户端
+*/
+func (c *Connection) StartWriter() {
+	defer fmt.Println(c.RemoteAddr().String(), "conne Writer exit!")
+
+	for {
+		select {
+		case data := <-c.msgChan:
+			//有数据要写给客户端
+			if _, err := c.Conn.Write(data); err != nil {
+				fmt.Println("Send Data error:, ", err, " Conn Writer exit")
+				return
+			}
+		case <-c.ExitBuffChan:
+			//conn已经关闭
+			return
+		}
+	}
+}
+
 //启动连接，让当前连接开始工作
 func (c *Connection) Start() {
-	//开启处理该链接读取到客户端数据之后的请求业务
+	//1 开启用户从客户端读取数据流程的Goroutine
 	go c.StartReader()
+	//2 开启用于写回客户端数据流程的Goroutine
+	go c.StartWriter()
 
 	for {
 		select {
@@ -144,11 +171,7 @@ func (c *Connection) SendMsg(msgId uint32, data []byte) error {
 	}
 
 	//写回客户端
-	if _, err := c.Conn.Write(msg); err != nil {
-		fmt.Println("Write msg id ", msgId, " error ")
-		c.ExitBuffChan <- true
-		return errors.New("conn Write error")
-	}
+	c.msgChan <- msg //将之前直接回写给conn.Write的方法 改为 发送给Channel 供Writer读取
 
 	return nil
 }
